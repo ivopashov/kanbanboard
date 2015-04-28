8b6f464c1d4a4e1df5b9eabb79e93f6460a03dbf
ivoPashov <ivopashov@abv.bg>
2015-04-28 19:22:27 +0300

    missed that one last time

--- Board.Test/angulartests/boardTests.js
+++ Board.Test/angulartests/boardTests.js
@@ -1,5 +1,5 @@
 ﻿
-(function() {
+(function () {
     describe('stringManipulationService test', function () {
         describe('when I call stringManipulationService.firstLetterToLowerCase', function () {
             beforeEach(module('app'));
@@ -19,13 +19,57 @@
     describe('stringManipulationService test', function () {
         describe('when I call stringManipulationService.splitWordsFromCamelCase', function () {
             beforeEach(module('app'));
-            
+
             it('proper split words from camelCase', inject(function (stringManipulationService) {
                 expect(stringManipulationService.splitWordsFromCamelCase("camelCase")).toEqual("Camel Case");
             }));
             it('proper split words from camelCase', inject(function (stringManipulationService) {
                 expect(stringManipulationService.splitWordsFromCamelCase("camelCaseTest")).toEqual("Camel Case Test");
             }));
+        });
+    });
+
+    describe('boardController', function () {
+        beforeEach(module('app'));
+
+        var $controller, boardService, notificationService, $modal, stringManipulationService;
+
+
+        beforeEach(inject(function (_$controller_, _boardService_, _notificationService_, _$modal_, _stringManipulationService_) {
+            // The injector unwraps the underscores (_) from around the parameter names when matching
+            $controller = _$controller_;
+            boardService = _boardService_;
+            notificationService = _notificationService_;
+            $modal = _$modal_;
+            stringManipulationService = _stringManipulationService_;
+        }));
+
+        describe('$scope.grade', function () {
+            it('sets the strength to "strong" if the password length is >8 chars', function () {
+                var $scope = {};
+                var controller = $controller('boardController', { $scope: $scope, boardService: boardService, stringManipulationService: stringManipulationService, $modal: $modal, notificationService: notificationService });
+                var event = {
+                    dest: {
+                        nodesScope: {
+                            $modelValue: [1, 2, 3]
+                        },
+                        index: 1
+                    },
+                    source: {
+                        nodesScope: {
+                            $modelValue: [1, 2, 3]
+                        },
+                        nodeScope: {
+                            $modelValue: 1
+                        },
+                        index: 0
+                    }
+                }
+                $scope.handleUnsuccessfullDrop(event);
+
+
+                expect("aaa").toEqual('aaa');
+            });
         });
     });
 })();
